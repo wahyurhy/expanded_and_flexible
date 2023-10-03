7388c9a1a2caaa680213842b6bf5da25af1ba777
wahyurhy <wahyurahayu82@gmail.com>
2023-10-03 18:23:27 +0700

    Using both, Expanded and Flexible

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,108 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Expanded & Flexible Home Page'),
+      home: const ExpandedFlexiblePage(title: 'Expanded & Flexible Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ExpandedFlexiblePage extends StatefulWidget {
+  const ExpandedFlexiblePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ExpandedFlexiblePage> createState() => _ExpandedFlexiblePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _ExpandedFlexiblePageState extends State<ExpandedFlexiblePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(child: Container(color: Colors.red,)),
-        Expanded(child: Container(color: Colors.orange,)),
-        Expanded(child: Container(color: Colors.yellow,)),
-        Expanded(child: Container(color: Colors.green,)),
-        Expanded(flex: 2, child: Container(color: Colors.blue,)),
-        Expanded(child: Container(color: Colors.indigo,)),
-        Expanded(child: Container(color: Colors.purple,)),
-      ],
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ExpandedWidget(),
+                FlexibleWidget()
+              ],
+            ),
+            Row(
+              children: [
+                ExpandedWidget(),
+                ExpandedWidget()
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                FlexibleWidget()
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                ExpandedWidget()
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
+class ExpandedWidget extends StatelessWidget {
+  const ExpandedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.teal,
+            border: Border.all(color: Colors.white)
+        ),
+        child: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Expanded',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 24
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  const FlexibleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.tealAccent,
+              border: Border.all(color: Colors.white),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Flexible',
+              style: TextStyle(
+                color: Colors.teal,
+                fontSize: 24
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
+
